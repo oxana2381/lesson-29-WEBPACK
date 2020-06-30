c1f01825abc66ecbd0264dc3e244c7de58d619de
Oxana Ciobanu <oxanochka23@yahoo.com>
2020-06-30 23:00:39 +0300

    added weather

--- src/js/api.js
+++ src/js/api.js
@@ -7,5 +7,5 @@ const API_KEY='07a4394bdd6059ae1a5ee2926eaa3126';
 //const API_KEY = '0ffd416aff3105ed18298e35a8ffaf0c';
 
 export function getWeatherByCity(city) {
-    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${API_KEY}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&&appid=${API_KEY}`)
 }
